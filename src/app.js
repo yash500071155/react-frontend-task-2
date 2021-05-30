ad9500309c51e368d982a689a7cb61775dfdd3d0
yash500071155 <75775134+yash500071155@users.noreply.github.com>
2021-05-30 13:49:28 +0530

    Create app.js

---
+++ src/app.js
@@ -0,0 +1,13 @@
+import React from "react"
+import WeatherPage from "./component/WeatherPage"
+import "./App.css"
+
+function App() {
+  return (
+    <>
+      <div className="main"><WeatherPage /></div>
+    </>
+  );
+}
+
+export default App;
